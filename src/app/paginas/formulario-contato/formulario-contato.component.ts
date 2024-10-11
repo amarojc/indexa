1f2735e1302723f05ce14e01c9f3051addfc24e9
amarojc <inf.amaro.jc@gmail.com>
2024-10-11 19:59:06 -0300

    Event Binding (Associação de eventos) implementado

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from "../../componentes/container/container.component";
@@ -9,11 +10,32 @@ import { SeparadorComponent } from "../../componentes/separador/separador.compon
   imports: [
     CommonModule,
     ContainerComponent,
-    SeparadorComponent
+    SeparadorComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
+  contatoForm: FormGroup;
+
+  constructor(){
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('Jorge'),
+      telefone: new FormControl('99 99999-999'),
+      email: new FormControl('amaro@mail.com'),
+      aniversario: new FormControl(),
+      redes: new FormControl('linkedin.com/amarojc'),
+      observacoes: new FormControl('Olá mundo!'),
+    })
+  }
+
+  salvarContato(){
+    console.log(this.contatoForm.value);
+  }
+
+  cancelar(){
+    console.log('Submissão cancelada!');
+  }
 }
